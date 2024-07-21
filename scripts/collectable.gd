c3a50b46f26b216d12e391616b846e8d35babac5
JakubCudak <jakub.cudak2000@gmail.com>
2024-07-21 19:56:01 +0200

    [19] added tooltip label

--- scripts/collectable.gd
+++ scripts/collectable.gd
@@ -2,8 +2,32 @@ extends Area2D
 
 @export var itemResource: InventoryItem
 @onready var inventory: Inventory = preload("res://assets/resources/inventory/player_inventory.tres")
+@onready var label = $Label
+
+var canPick = false
+const PICKUP_KEY = 'E'
+
+func _ready():
+	set_process_input(false)
+	label.text = "["+PICKUP_KEY+"]"
+	label.visible = false
 
 func _on_body_entered(body):
-	if(body.name == "Player" && inventory.has_space()):
-		inventory.insert(itemResource)
-		queue_free()
+	if body.name == "Player":
+		label.visible = true
+		canPick = true
+		set_process_input(true)
+
+func _on_body_exited(body):
+	if body.name == "Player":
+		label.visible = false
+		canPick = false
+		set_process_input(false)
+
+func _input(event):
+	if event.is_action_pressed("pickup_item") and canPick:
+		if inventory.has_space():
+			inventory.insert(itemResource)
+			queue_free()
+		else:
+			pass

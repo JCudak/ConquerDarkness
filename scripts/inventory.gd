cfd8e73a78a3f81d02008d203f18b0419cfaaa2b
JakubCudak <jakub.cudak2000@gmail.com>
2024-07-25 20:39:20 +0200

    [13] added inventory item moving, swapping and reseting

--- scripts/inventory.gd
+++ scripts/inventory.gd
@@ -3,17 +3,27 @@ extends Resource
 class_name Inventory
 signal updated
 
-@export var items: Array[InventoryItem]
+@export var slots: Array[InventorySlot]
 
 func insert(item: InventoryItem):
-	for i in range(items.size()):
-		if!items[i]:
-			items[i] = item
+	for i in range(slots.size()):
+		if!slots[i].item:
+			slots[i].item = item
 			updated.emit()
 			break
 
 func has_space():
-	for i in range(items.size()):
-		if!items[i]:
+	for i in range(slots.size()):
+		if!slots[i].item:
 			return true
 	return false
+
+func removeSlot(inventorySlot: InventorySlot):
+	var index = slots.find(inventorySlot)
+	if index < 0: return
+	
+	slots[index] = InventorySlot.new()
+
+
+func insertSlot(index: int, inventorySlot: InventorySlot):
+	slots[index] = inventorySlot

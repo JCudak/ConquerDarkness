606ded6f7e3d20c085bdf3d03a46dc9c11d90406
JakubCudak <jakub.cudak2000@gmail.com>
2024-07-26 23:48:10 +0200

    [15] added rune HUD and some rune updates

--- scripts/canvas_layer.gd
+++ scripts/canvas_layer.gd
@@ -1,10 +1,12 @@
 extends CanvasLayer
 
 @onready var inventory = $SlotsContainers/InventoryGUI
+@onready var rune_equip = $SlotsContainers/RuneEquipGUI
 @onready var settings = $InputSettings
 
 func _ready():
 	inventory.close()
+	rune_equip.close()
 	inventory.locked = true
 	settings.close()
 	
@@ -13,9 +15,11 @@ func _input(event):
 		if inventory.isOpen:
 			inventory.locked = true
 			inventory.close()
+			rune_equip.close()
 		else:
 			inventory.locked = false
 			inventory.open()
+			rune_equip.open()
 	if event.is_action_pressed("toggle_settings"):
 		if settings.isOpen:
 			settings.close()
--- scripts/inventory_&_hotbar/inventory_item.gd
+++ scripts/inventory_&_hotbar/inventory_item.gd
@@ -2,7 +2,10 @@ extends Resource
 
 class_name InventoryItem
 
+enum CollectableType {RUNE, POTION}
+
 @export var name: String = ""
+@export var type: CollectableType
 @export var texture: Texture2D
 
 

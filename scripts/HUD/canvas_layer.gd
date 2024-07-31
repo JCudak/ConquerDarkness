61b18ed6f03e793a094a57801e2cdce2fb931c53
JakubCudak <jakub.cudak2000@gmail.com>
2024-07-31 02:50:45 +0200

    [97] added map

--- scripts/HUD/canvas_layer.gd
+++ scripts/HUD/canvas_layer.gd
@@ -2,7 +2,11 @@ extends CanvasLayer
 
 @onready var inventory = $HUD/InventoryGUI
 @onready var rune_equip = $HUD/RuneEquipGUI
+@onready var hud = $HUD
 @onready var settings = $InputSettings
+@export var camera_2d: Camera2D
+
+var is_map_open: bool = false
 
 func _ready():
 	inventory.close()
@@ -11,7 +15,7 @@ func _ready():
 	settings.close()
 	
 func _input(event):
-	if event.is_action_pressed("toggle_inventory"):
+	if event.is_action_pressed("toggle_inventory") and not get_tree().paused:
 		if inventory.isOpen:
 			inventory.locked = true
 			inventory.close()
@@ -20,8 +24,15 @@ func _input(event):
 			inventory.locked = false
 			inventory.open()
 			rune_equip.open()
-	if event.is_action_pressed("toggle_settings"):
-		if settings.isOpen:
-			settings.close()
-		else:
-			settings.open()
+	
+	if event.is_action_pressed("toggle_map"):
+		if is_map_open:
+			get_tree().paused = false
+			is_map_open = false
+			camera_2d.zoom = camera_2d.zoom*3
+			hud.visible = true
+		elif not get_tree().paused:
+			is_map_open = true
+			camera_2d.zoom = camera_2d.zoom/3
+			get_tree().paused = true
+			hud.visible = false
